7c1c245c21247ed4f43503e197fb931ec8190b73
NothJosue <thedarkm04@gmail.com>
2025-08-03 10:31:28 -0500

    Revert "revisar manera de evitar usar doble exception en clase WheaterSErvice"
    This reverts commit 60cd4605591547cad71e74089acd0df6226bdfbc.
    
    	deleted:    .idea/uiDesigner.xml
    	modified:   src/main/java/Main.java
    	deleted:    src/main/java/model/Activity.java
    	deleted:    src/main/java/model/Wheater.java
    	deleted:    src/main/java/service/ActivityService.java
    	deleted:    src/main/java/service/WheaterService.java
    	deleted:    target/classes/Main.class
    	deleted:    target/classes/model/Activity.class
    	deleted:    target/classes/model/Wheater.class
    	deleted:    target/classes/service/ActivityService.class
    	deleted:    target/classes/service/WheaterService.class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,5 @@
-import service.WheaterService;
-
 public class Main {
     public static void main(String[] args) {
-
-        //test.
-        //Reemplazar con %20 como en el ejemplo, los nombres de las ciudades que tengan espacios.
-
-        WheaterService wheaterService=new WheaterService();
-
-        System.out.println(wheaterService.findWheater("sfgdhgfd"));
-
+        System.out.println("Hello world!");
     }
 }
